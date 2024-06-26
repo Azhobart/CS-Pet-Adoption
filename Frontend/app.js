f8921b293fc69462d5b11e43df6e1682640583d0
SegaSonic-88 <gavin.talbot@utsuccess.org>
2024-06-26 10:23:53 -0600

    added frontend

---
+++ Frontend/app.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+    // contains data - variables, arrays, etc.
+    data() {
+      return {
+
+        
+      };
+    },
+
+    methods: {
+      
+
+      
+
+
+    },
+
+    
+
+    created: function() {
+        console.log("vue app loaded");
+    }
+}).mount("#app");
