4221f27ede37e73ef97325f10b482d70df19e50b
SegaSonic-88 <gavin.talbot@utsuccess.org>
2024-06-26 11:47:52 -0600

    add get and post for /application

--- Frontend/app.js
+++ Frontend/app.js
@@ -6,11 +6,19 @@ Vue.createApp({
       return {
         page: 1,
         pets: [],
-        name: "",
+        applications: [],
+        //pet key info
+        petName: "",
         species: "",
         breed: "",
         age: 0,
         gender: "",
+        //application key info
+        userName: "",
+        phoneNumber: 0,
+        email: "",
+        petID: 0,
+
 
 
 
@@ -22,7 +30,7 @@ Vue.createApp({
             this.page = pageNum;
       },
 
-      getPets: async function() {
+      getPetListings: async function() {
         let response = await fetch(`${URL}/pets`);
         let data = await response.json();
         this.pets = data;
@@ -30,35 +38,83 @@ Vue.createApp({
 
       },
 
-      postPets: async function() {
+      createPetListing: async function() {
         const formData = new FormData();
-        formData.append("name", this.name);
+        formData.append("name", this.petName);
         formData.append("species", this.species);
         formData.append("breed", this.breed);
         formData.append("age", this.age);
         formData.append("gender", this.name);
 
 
-        let requestOption = {
+        let requestOptions = {
             method: "POST",
             body: formData,
-        }
+        };
 
-        let respsone = await fetch (`${URL}/posts`, requestOption);
+        let response = await fetch (`${URL}/posts`, requestOptions);
         const data = await response.json();
         this.pets.push(data);
 
 
       },
-      
+    
+      deletePetListing: async function(index = null) {
+        let requestOptions = {
+            method: "DELETE",
+        };
+
+        let petID = this.pets[index]._id;
+
+        let response = await fetch(`${URL}/pets/${petId}`, requestOptions);
+
+        if (response.status === 204) {
+            this.pets.splice(index, 1);
+
+        } else {
+            alert("Failed to Remove Pet");
+        }
+      },
+
+
+
+
+
+      getApplications: async function() {
+        let response = await fetch(`${URL}/applications`);
+        let data = await response.json();
+        this.applications = data;
+        console.log(this.applications)
+
+      },
+
+      postApplication: async function() {
+        const formData = new FormData();
+        formData.append("name", this.userName);
+        formData.append("phone_number", this.phoneNumber);
+        formData.append("email", this.email);
+        formData.append("pet_id", this.petID);
+
+        let requestOptions = {
+            method: "POST",
+            body: formData,
+        };
+
+        let response = await fetch(`${URL}/applications`, requestOptions);
+        let data = response.json();
+        this.applications.push(data);
+
+      }
 
 
     },
+
 
     
 
     created: function() {
         console.log("vue app loaded");
-        this.getPets();
+        this.getPetListings();
+        this.getApplications();
     }
 }).mount("#app");
